d965efb0e55c306846de37b978cd52c5b22d5f36
xibei <1228019939@qq.com>
2026-02-25 16:27:09 +0800

    Update frida_sign_api.py

--- server/frida_sign_api.py
+++ server/frida_sign_api.py
@@ -27,7 +27,7 @@ class FlaskServer:
         ret = ""
         for i in range(n):
             num = random.randint(0, 9)
-            # num = chr(random.randint(48,57))#ASCII表示数字
+            # num = chr(random.randint(48,57))#ASCII
             letter = chr(random.randint(97, 122))  # 取小写字母
             Letter = chr(random.randint(65, 90))  # 取大写字母
             s = str(random.choice([num, letter, Letter]))
@@ -82,3 +82,4 @@ class FlaskServer:
 
         print("开启端口服务：", flask_prot)
         app.run(port=int(flask_prot))
+
